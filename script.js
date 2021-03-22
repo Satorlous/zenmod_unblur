ccffe79bdfd8b0ace33c309dabd309654b19288b
Satorlous <55993655+Satorlous@users.noreply.github.com>
2021-03-22 21:10:48 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Unblur Jija's images
+// @namespace    https://vk.com/id85442235
+// @version      0.1
+// @description  Убирает замыливание с изображений жидкостей на сайте tmn.zenmod.ru
+// @author       Satorlous
+// @connect      zenmod.ru
+// @connect      *.zenmod.ru
+// @include     http://*
+// @include     https://*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    let jijaBlocks = document.querySelectorAll('.has_nicotine');
+    for(let i = 0; i < jijaBlocks.length; i++) {
+        jijaBlocks[i].classList.remove("has_nicotine");
+    }
+})();
